df46e9a523d7578695923b9969a70191c816ba02
theodortruffer <theodortruffer@bluewin.ch>
2013-10-22 16:40:49 +0200

    you can choose an item in the mensa list now and it leads you to "MensaDetails". But "MensaDetails" doesnt show the right data yet.

--- ProjectFiles/mensaunibe/src/com/ese2013/mensaunibe/MensaDetailsFragment.java
+++ ProjectFiles/mensaunibe/src/com/ese2013/mensaunibe/MensaDetailsFragment.java
@@ -1,9 +1,26 @@
 package com.ese2013.mensaunibe;
 
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class MensaDetailsFragment extends Fragment {
 	
+	public MensaDetailsFragment(){
+		//empty constructor required for Fragment subclasses
+	}
 	
-
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_mensadetails,
+				container, false);
+		
+	final ListView listview = (ListView) rootView
+			.findViewById(R.id.menus);
+	
+	return rootView;
+	}
 }
