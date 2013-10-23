791408807c0f99a61fbdef952cda50b45a06368f
exside <luk@exside.ch>
2013-10-23 15:25:43 +0200

    Refactored Classes / Names, removed dummy project

--- ProjectFiles/mensaunibe/src/com/ese2013/mensaunibe/util/LocalDataLoader.java
+++ ProjectFiles/mensaunibe/src/com/ese2013/mensaunibe/util/LocalDataLoader.java
@@ -3,7 +3,7 @@ package com.ese2013.mensaunibe.util;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.ese2013.mensaunibe.Home;
+import com.ese2013.mensaunibe.ActivityMain;
 
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -22,7 +22,7 @@ public class LocalDataLoader extends AsyncTask<Void, Void, Set<Integer>> {
 	@Override
 	protected Set<Integer> doInBackground(Void... params) {
 		SharedPreferences prefs = PreferenceManager
-				.getDefaultSharedPreferences(Home.getContextOfApp());
+				.getDefaultSharedPreferences(ActivityMain.getContextOfApp());
 		Set<String> favs = prefs.getStringSet(FAVORITE_MENSAS_KEY,
 				new HashSet<String>());
 		Set<Integer> result = new HashSet<Integer>();
