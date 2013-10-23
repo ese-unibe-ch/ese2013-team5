f9439c6ea22dc7ccc4626b216a3b906551b4da39
exside <luk@exside.ch>
2013-10-23 16:54:18 +0200

    Added custom list adapter for mensalist and make tap on mensa to show details work

--- ProjectFiles/mensaunibe/src/com/ese2013/mensaunibe/FragmentMensaList.java
+++ ProjectFiles/mensaunibe/src/com/ese2013/mensaunibe/FragmentMensaList.java
@@ -18,13 +18,14 @@ import android.widget.AdapterView.OnItemClickListener;
 
 import com.ese2013.mensaunibe.model.Mensa;
 import com.ese2013.mensaunibe.model.Model;
+import com.ese2013.mensaunibe.util.CustomMensalistAdapter;
 
 /**
  * Fragment that appears in the "content_frame", shows mensalist
  */
 public class FragmentMensaList extends Fragment {
-	private SimpleAdapter adapter;
-	private Activity home;
+	private CustomMensalistAdapter adapter;
+	private Activity main;
 
 	public FragmentMensaList() {
 		// Empty constructor required for fragment subclasses
@@ -35,61 +36,46 @@ public class FragmentMensaList extends Fragment {
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_mensalist,
 				container, false);
-		home = this.getActivity();
-		Model model = ((ActivityMain) home).model;
+		main = this.getActivity();
+		Model model = ((ActivityMain) main).model;
 	
 		ArrayList <Mensa> mensalist = model.getMensas();
 		
-
+//		Toast toast = Toast.makeText(main.getBaseContext(),
+//				"Hier werden alle Mensas im †berblick angezeigt",
+//				Toast.LENGTH_LONG);
+//		toast.show();
 		// get the list view from the layout into a variable, it's important
 		// to fetch it from the rootView
-		final ListView listview = (ListView) rootView
-				.findViewById(R.id.mensalist);
+		final ListView listview = (ListView) rootView.findViewById(R.id.mensalist);
 
-		ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
-		//ArrayList<Integer> mensaIds = new ArrayList<Integer>();
-		HashMap<String, String> item;
-		for (Mensa mensa : mensalist) {
-			item = new HashMap<String, String>();
-			item.put("line1", mensa.getName());
-			item.put("line2", mensa.getStreet() + " | " + mensa.getPlz());
-			//mensaIds.add(mensa.getId());
-			list.add(item);
-		}
-
-		adapter = new SimpleAdapter(inflater.getContext(), list,
-				R.layout.list_mensalist_item, new String[] { "line1",
-						"line2"}, new int[] { R.id.line1, R.id.line2});
+		adapter = new CustomMensalistAdapter(getActivity(), this, mensalist, R.layout.list_mensalist_item);
 
 		listview.setAdapter(adapter);
-		listview.setOnItemClickListener(new MensalistItemClickListener());
-		((ActivityMain) home).listView = listview;
+		//listview.setOnItemClickListener(new MensalistItemClickListener());
+		((ActivityMain) main).listView = listview;
 		//for(int i = 0; i < listview.getCount(); i++){
 			//Object view = listview.getItemAtPosition(i);
 			//((Home) home).mensalistItemIds.add(view.getId());
 		//}
-		Toast toast = Toast.makeText(inflater.getContext(),
-				"Hier werden alle Mensas im Überblick angezeigt",
-				Toast.LENGTH_LONG);
-		toast.show();
+		
 		return rootView;
 	}
 	
-	private class MensalistItemClickListener implements OnItemClickListener {
-
-		@Override
-		public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
-			selectItem(position);
-		}
-
-	}
+//	private class MensalistItemClickListener implements OnItemClickListener {
+//
+//		@Override
+//		public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+//			selectItem(position);
+//		}
+//
+//	}
 
 	public void selectItem(int position) {
-		((ActivityMain) home).currentMensa = ((ActivityMain) home).model.getMensas().get(position);
+		((ActivityMain) main).currentMensa = ((ActivityMain) main).model.getMensas().get(position);
 		Fragment fragment = new FragmentMensaDetails();
 		FragmentManager fragmentManager = getFragmentManager();
-		fragmentManager.beginTransaction()
-				.replace(R.id.content_frame, fragment).commit();
+		fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
 	}
 
 
