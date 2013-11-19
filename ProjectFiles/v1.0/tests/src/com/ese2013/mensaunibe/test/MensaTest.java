6587dc0368879c1e1c597d336030c4056fcb9b6d
exside <luk@exside.ch>
2013-11-19 16:47:22 +0100

    Added tests to v1.0 and added installable .apk file

---
+++ ProjectFiles/v1.0/tests/src/com/ese2013/mensaunibe/test/MensaTest.java
@@ -0,0 +1,27 @@
+package com.ese2013.mensaunibe.test;
+
+import junit.framework.TestCase;
+
+public class MensaTest extends TestCase {
+
+	public MensaTest() {
+		super();
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Code to set up test fixtures goes here
+		super.setUp();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		// TODO Code to clean up and reset fixtures goes here
+		super.tearDown();
+	}
+
+	public void testPreconditions() {
+		// TODO Code to test if everything is setup correctly goes here
+	}
+
+}
